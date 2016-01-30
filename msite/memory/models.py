18099fc04e6c00d5bbf1ea59c54043f4c15083f2
leovt <leonhard.vogt@gmx.ch>
2016-01-31 00:36:54 +0100

    docstrings

--- msite/memory/models.py
+++ msite/memory/models.py
@@ -2,16 +2,23 @@ from django.db import models
 import random
 
 def random_id(length):
+    """return a random string of given length
+
+    the string is composed of numbers and lowercase letters
+    """
     CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789'
     return ''.join(random.choice(CHARACTERS) for _ in range(length))
 
 def _default_urlid():
+    """provides the default for Game.urlid"""
     return random_id(Game._meta.get_field('urlid').max_length)
 
 def _default_secretid():
+    """provides the default for Player.secretid"""
     return random_id(Player._meta.get_field('secretid').max_length)
 
 class Game(models.Model):
+    """represent a game"""
     STATUS_WAIT_FOR_PLAYERS = 0
     STATUS_NO_CARD_SHOWN = 1
     STATUS_ONE_CARD_SHOWN = 2
@@ -24,13 +31,22 @@ class Game(models.Model):
     NUMBER_OF_PAIRS = 18
     
     urlid = models.CharField(max_length=10, unique=True, default=_default_urlid)
+    """the id of the game used in urls, should be only known to the players"""
+
     status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_WAIT_FOR_PLAYERS)
+    """the status of the game"""
+
     current_player = models.ForeignKey('Player', on_delete=models.PROTECT, related_name='+', null=True, blank=True)
+    """the current player, i.e. the player who is currently allowed to perform actions"""
 
     def __str__(self):
         return self.urlid
     
     def new_game(self):
+        """reset the cards used in the game
+
+        clears the cards used for the game and sets up random cards in pairs
+        """
         images = 2 * random.sample(list(Image.objects.all()), Game.NUMBER_OF_PAIRS)
         random.shuffle(images)
         self.save()
@@ -39,18 +55,31 @@ class Game(models.Model):
 
 class Player(models.Model):
     secretid = models.CharField(max_length=10, unique=True, default=_default_secretid)
+    """the secret id for identifying the player"""
+
     name = models.CharField(max_length=30)
+    """name or nickname of the player"""
+
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='players')
+    """the game in which this player is participating"""
+
     score = models.IntegerField(default=0)
+    """the score in the current game"""
 
     def __str__(self):
         return self.name
     
 class Image(models.Model):
+    """represent an image which appears on the front of the card"""
     def __str__(self):
         return 'Image%d' % self.id
 
 class Card(models.Model):
+    """represent a card in a game"""
+
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='cards')
+    """the game in which this card is used"""
+
     image = models.ForeignKey(Image, on_delete=models.PROTECT, related_name='+')
+    """the image on the front of the card"""
     
