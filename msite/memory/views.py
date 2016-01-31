2b9b8568d89250f17d1c7c97b021d074b85e6a69
leovt <leonhard.vogt@gmx.ch>
2016-01-31 19:19:53 +0100

    added identification of player by cookie

--- msite/memory/views.py
+++ msite/memory/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views.generic import View
+from django.core.urlresolvers import reverse
 
-from .models import Game
+from .models import Game, Player
 
 def game(request, urlid):
     game = get_object_or_404(Game, urlid=urlid)
+
+    player_id = request.COOKIES.get('player_id', '')
+    try:
+        player = game.players.get(secretid=player_id)
+    except Player.DoesNotExist:
+        player = None
+
+
     return render(request, 'memory/game.html', {
-        'game': game})
+        'game': game, 'player':player})
 
 class Welcome(View):
     def post(self, request):
         '''create a new game and redict to it'''
+        name = request.POST.get("name", "")
+
+        if not name:
+            return render(request, 'memory/newgame.html', {'error': 'please enter a name'})
+
+        player = Player(name=name)
+
         game = Game()
         game.save()
-        return HttpResponseRedirect(game.get_absolute_url())
+        player = Player(game=game, name=name)
+        player.save()
+
+        response = HttpResponseRedirect(game.get_absolute_url())
+        response.set_cookie('player_id', player.secretid, path=game.get_absolute_url())
+        return response
 
     def get(self, request):
         '''show a form to enable a user to create a new game'''
