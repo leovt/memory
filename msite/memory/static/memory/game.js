858d1886294c2b93139f4d7458f4f88ebf540472
leovt <leonhard.vogt@gmx.ch>
2016-02-11 22:34:09 +0100

    hooked javascript in the form

--- msite/memory/static/memory/game.js
+++ msite/memory/static/memory/game.js
@@ -13,6 +13,17 @@ function reqListener () {
 	  }
 	  element.textContent = player.name + ": " + player.score 
   }
+  for (cardID in response.cards) {
+	  card = response.cards[cardID]
+	  element = document.getElementById(cardID)
+	  element.style.backgroundPosition = card.bgpos
+	  if (card.visible){
+		  element.style.visibility = "visible";
+	  }
+	  else{
+		  element.style.visibility = "hidden";
+	  }
+  }
 }
 
 function sendRequest() {
@@ -21,4 +32,27 @@ function sendRequest() {
 	oReq.open("GET", "#");
 	oReq.setRequestHeader("Accept","application/json");
 	oReq.send();
+}
+
+function clickCard(e) {
+	e.preventDefault();/*
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", reqListener);
+	oReq.open("POST", "#");
+	oReq.setRequestHeader("Accept","application/json");
+	oReq.send();*/
+	console.log(this.value)
+	return false;
+}
+
+function main(){
+	var form = document.getElementById("game-form")
+	form.onsubmit = function(e){e.preventDefault(); return false};
+	for (name in form.elements) {
+		var element = form.elements[name];
+		if (element.type == "submit"){
+			element.onclick = clickCard;
+		}
+	} 
+	
 }
